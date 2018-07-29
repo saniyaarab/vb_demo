214a63aef7cf7726d749c31644f8ab571c619748
saniyaarab <41695674+saniyaarab@users.noreply.github.com>
2018-07-29 16:12:04 -0400

    Add files via upload

---
+++ Rainfall Statistics/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblAvg = New System.Windows.Forms.Label()
+        Me.lblMin = New System.Windows.Forms.Label()
+        Me.lblMax = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(12, 12)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(290, 251)
+        Me.lstOutput.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(346, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Monthly Rainfall Statistics"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Location = New System.Drawing.Point(317, 65)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(384, 23)
+        Me.lblTotal.TabIndex = 2
+        '
+        'lblAvg
+        '
+        Me.lblAvg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAvg.Location = New System.Drawing.Point(317, 109)
+        Me.lblAvg.Name = "lblAvg"
+        Me.lblAvg.Size = New System.Drawing.Size(384, 23)
+        Me.lblAvg.TabIndex = 3
+        '
+        'lblMin
+        '
+        Me.lblMin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMin.Location = New System.Drawing.Point(317, 155)
+        Me.lblMin.Name = "lblMin"
+        Me.lblMin.Size = New System.Drawing.Size(384, 23)
+        Me.lblMin.TabIndex = 4
+        '
+        'lblMax
+        '
+        Me.lblMax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMax.Location = New System.Drawing.Point(317, 207)
+        Me.lblMax.Name = "lblMax"
+        Me.lblMax.Size = New System.Drawing.Size(384, 23)
+        Me.lblMax.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(27, 276)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(125, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Input Monthly Rainfall"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(223, 276)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(120, 23)
+        Me.btnDisplay.TabIndex = 7
+        Me.btnDisplay.Text = "Display Stats"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(409, 276)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(119, 23)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(586, 276)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(115, 23)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(747, 311)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblMax)
+        Me.Controls.Add(Me.lblMin)
+        Me.Controls.Add(Me.lblAvg)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Name = "Form1"
+        Me.Text = "Rainfall Statistics"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents lblAvg As System.Windows.Forms.Label
+    Friend WithEvents lblMin As System.Windows.Forms.Label
+    Friend WithEvents lblMax As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Rainfall Statistics/Form1.vb
@@ -0,0 +1,156 @@
+﻿Option Strict On
+
+Public Class Form1
+
+    Dim strMonth() As String = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+    Const intMAX_SUBSCRIPT As Integer = 11
+    'Array to hold rainfall
+    Dim decRainfall(intMAX_SUBSCRIPT) As Decimal
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+
+        If lstOutput.Items.Count = 0 Then
+            MessageBox.Show("Click on Input Monthly Rainfall button to enter data in order to view Satistics")
+            Exit Sub
+        End If
+
+        'Other local variable
+        Dim decTotal As Decimal ' to hold total 
+        Dim decAverage As Decimal ' to hold the average
+        Dim decMax As Decimal ' to hold to maximum rainfall
+        Dim decMin As Decimal ' to hold minimum rainfall
+        Dim intMaxId As Integer
+        Dim strMaxMonth As String
+        Dim intMinId As Integer
+        Dim strMinMonth As String
+
+
+        intMaxId = indexOfMaxValue(decRainfall)
+        intMinId = indexOfMinValue(decRainfall)
+
+        'Get the total rainfall
+        decTotal = TotalArray(decRainfall)
+        decAverage = AverageArray(decRainfall)
+        decMax = decRainfall(intMaxId)
+        strMaxMonth = strMonth(intMaxId)
+        decMin = decRainfall(intMinId)
+        strMinMonth = strMonth(intMinId)
+
+        'display the result
+        lblTotal.Text = "The total annual rainfall was " & decTotal.ToString()
+        lblAvg.Text = "The average monthly rainfall was " & decAverage.ToString("n2")
+        lblMax.Text = "The maximum monthly rainfall was " & decMax.ToString() & "(" & strMaxMonth & ")"
+        lblMin.Text = "The minimum monthly rainfall was " & decMin.ToString() & "(" & strMinMonth & ")"
+
+    End Sub
+
+    Sub GetRainfallData(ByRef decRainfall() As Decimal)
+        Dim intcount As Integer = 0 'loop counter set to 0
+
+        'Fill decrainfall array from data enetered by user
+        Do While intcount < decRainfall.Length
+            Try
+                Dim strUserInput As String = InputBox("Enter Rainfall for the Month of " & strMonth(intcount))
+                If strUserInput = "" Then
+                    MessageBox.Show("If no numeric value is entered then by default it will be zero for " & strMonth(intcount), "Validation Input Error", MessageBoxButtons.OK)
+                    strUserInput = "0"
+                End If
+                If CDec(strUserInput) < 0 Then
+                    MessageBox.Show("Please enter non negative numbers")
+                    strUserInput = "0"
+                End If
+                'get rainfall for the month
+                decRainfall(intcount) = CDec(strUserInput)
+                intcount += 1
+            Catch ex As Exception
+                'display error message for invalid value
+                MessageBox.Show("Enter a numeric value.")
+            End Try
+        Loop
+
+    End Sub
+
+    Function TotalArray(ByVal decValues() As Decimal) As Decimal
+        Dim decTotal As Decimal = 0 ' accumulator
+        Dim intCount As Integer 'loop counter
+
+        'Calculate the total of the array's elements
+        For intCount = 0 To (decValues.Length - 1)
+            decTotal += decValues(intCount)
+        Next
+        Return decTotal
+
+    End Function
+
+    Function AverageArray(ByVal decValues() As Decimal) As Decimal
+        Return TotalArray(decValues) / decValues.Length
+    End Function
+
+    Function indexOfMaxValue(ByRef decValues() As Decimal) As Integer
+        Dim intcount As Integer ' loop counter
+        Dim decMax As Decimal ' to hold max value
+        Dim intMaxid As Integer
+        decMax = decValues(0)
+
+        'Search for the maximum value
+        For intcount = 1 To (decValues.Length - 1)
+            If decValues(intcount) > decMax Then
+                decMax = decValues(intcount)
+                intMaxid = intcount
+            End If
+        Next
+
+        Return intMaxid
+    End Function
+
+    Function indexOfMinValue(ByRef decValues() As Decimal) As Integer
+        Dim intcount As Integer ' loop counter
+        Dim decMin As Decimal ' to hold min value
+        Dim intMinId As Integer
+
+        decMin = decValues(0)
+
+        'search for min value in array
+        For intcount = 1 To (decValues.Length - 1)
+            If decValues(intcount) < decMin Then
+                decMin = decValues(intcount)
+                intMinId = intcount
+            End If
+        Next
+
+        Return intMinId
+
+    End Function
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lstOutput.Items.Clear()
+        lblAvg.Text = String.Empty
+        lblTotal.Text = String.Empty
+        lblMin.Text = String.Empty
+        lblMax.Text = String.Empty
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'clear existing list
+        lstOutput.Items.Clear()
+
+        ' To get the rainfall from user
+        GetRainfallData(decRainfall)
+
+        'Display the Rainfall statistics Header
+
+        lstOutput.Items.Add("Monthly Rainfall Input")
+        lstOutput.Items.Add("----------------------------")
+
+        'display the array in listbox
+        For intcount = 0 To intMAX_SUBSCRIPT
+            lstOutput.Items.Add("Rainfall for " & strMonth(intcount) & " = " & decRainfall(intcount))
+        Next
+
+    End Sub
+End Class
+
